08d6290fefada3cb289e09f1655739a4eca835ae
Bilal Ali <bilalali2@hotmail.com>
2022-03-15 19:20:45 -0400

    refactor Content component into separate Part components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,16 +4,17 @@ const Header = (props) => <h1>{props.course}</h1>;
 // renders parts of the course
 const Content = (props) => (
   <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
   </>
+);
+
+// renders information about each part
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
 );
 
 // renders total number of exercises
