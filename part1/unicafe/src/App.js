a01832fcd5744b852c9126d1f3fab38a56d7b201
Bilal Ali <bilalali2@hotmail.com>
2022-03-22 16:08:50 -0400

    add components to replicate unicafe app

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,41 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  // set state variables
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // functions to increment state variables
+  // state variables don't need to be passed in because they're global variables (within the scope of App)
+  const incrGood = () => setGood(good + 1);
+  const incrNeutral = () => setNeutral(neutral + 1);
+  const incrBad = () => setBad(bad + 1);
+
   return (
-    
+    <div>
+      <h1>give feedback</h1>
+      <FeedbackButton text="good" handler={incrGood} />
+      <FeedbackButton text="neutral" handler={incrNeutral} />
+      <FeedbackButton text="bad" handler={incrBad} />
+      <h1>statistics</h1>
+      <DisplayFeedback text="good" score={good} />
+      <DisplayFeedback text="neutral" score={neutral} />
+      <DisplayFeedback text="bad" score={bad} />
+    </div>
   );
-}
+};
+
+// accepts reference to function that updates state variable
+const FeedbackButton = ({ text, handler }) => (
+  <button onClick={handler}>{text}</button>
+);
+
+// accepts state variable as prop to display
+const DisplayFeedback = ({ text, score }) => (
+  <div>
+    {text} {score}
+  </div>
+);
 
 export default App;
