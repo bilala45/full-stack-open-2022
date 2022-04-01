99b294823415ebd133c9fd727a3093f09705713c
Bilal Ali <bilalali2@hotmail.com>
2022-04-01 14:21:01 -0400

    add method to fetch persons data from db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Numbers from "./components/Numbers";
 import PersonForm from "./components/PersonForm";
+import axios from "axios";
 
 const App = () => {
   // state variables
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040 - 1234567", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  // retrieve data from db.json
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data))
+      .catch(() => console.log("Error fetching resource."));
+  }, []);
+
+  console.log(persons);
 
   // handles form submission by adding input to persons and resetting newName
   const handleSubmit = (event) => {
