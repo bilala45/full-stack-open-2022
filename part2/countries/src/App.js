27b6f07387100505c24761f208617630a5a28477
Bilal Ali <bilalali2@hotmail.com>
2022-04-01 17:54:11 -0400

    search countries data from restcountries API

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Countries from "./Countries";
+
 const App = () => {
-  return <div>Hello, world!</div>;
+  // stores country query
+  const [query, setQuery] = useState("");
+  // stores country data retrieved from restcountries api
+  const [countries, setCountries] = useState([]);
+
+  // request for countries data
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => setCountries(response.data))
+      .catch(() => console.log("error"));
+  }, []);
+
+  return (
+    <>
+      <form>
+        find countries:{" "}
+        <input
+          onChange={(event) => setQuery(event.target.value)}
+          value={query}
+        />
+      </form>
+      <Countries countries={countries} query={query} />
+    </>
+  );
 };
 
 export default App;
